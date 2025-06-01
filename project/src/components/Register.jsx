7fd911178343d47801545069bc0c6b9b6173a7c3
Tompi-dev <tomiris.kassymova.98@gmail.com>
2025-06-01 17:27:22 +0500

    Min mistake fixed

--- project/src/components/Register.jsx
+++ project/src/components/Register.jsx
@@ -14,7 +14,7 @@ export default function Register() {
     setError('');
 
     try {
-      const response = await fetch(`'${API_BASE_URL}/api/register'`, {
+      const response = await fetch(`${API_BASE_URL}/api/register`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
