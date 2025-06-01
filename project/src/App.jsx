9dec4255ffeecd169f192e3a91c0222075095d84
Tompi-dev <tomiris.kassymova.98@gmail.com>
2025-06-02 01:28:58 +0500

    fixed routers

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,93 +1,9 @@
-// import {
-//   Route,
-//   createBrowserRouter,
-//   createRoutesFromElements,
-//   RouterProvider,
-// } from 'react-router-dom';
-// import MainLayout from './layouts/MainLayout';
-// import LoginPage from './pages/LoginPage';
-// import HomePage from './pages/HomePage';
-// import Register from './components/Register'; 
 
-// const router = createBrowserRouter(
-//   createRoutesFromElements(
-//     <Route path='/' element={<MainLayout />}> 
-//       <Route index element={<LoginPage />} />
-//           <Route path="/" element={<LoginPage />} />
-//       <Route path='home' element={<HomePage />} />
-//       <Route path='register' element={<Register />} />
-//     </Route>
-//   )
-// );
-
-// const App = () => {
-//   return <RouterProvider router={router} />;
-// };
-
-// export default App;
-
-
-import {
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
-} from 'react-router-dom';
-
-import MainLayout from './layouts/MainLayout';
-
-import HomePage from './pages/HomePage';
-import Register from './components/Register';
-import Auth from './components/Auth';
-import GuestRoute from './pages/GuestRoute ';
-import PrivateRoute from './pages/PrivateRoute';
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<MainLayout />}>
-      <Route
-        index
-        element={
-          <GuestRoute>
-            <Auth />
-          </GuestRoute>
-        }
-      />
-      <Route
-        path='login'
-        element={
-          <GuestRoute>
-            <Auth />
-          </GuestRoute>
-        }
-      />
-      <Route
-        path='register'
-        element={
-          <GuestRoute>
-            <Register />
-          </GuestRoute>
-        }
-      />
-      <Route
-        path='home'
-        element={
-          <PrivateRoute>
-            <HomePage />
-          </PrivateRoute>
-        }
-      />
-    </Route>
-  )
-);
+import { RouterProvider } from 'react-router-dom';
+import router from './routers';
 
 const App = () => {
   return <RouterProvider router={router} />;
 };
 
 export default App;
-
-
-
-
-
---
+++ project/src/router.js
@@ -0,0 +1,54 @@
+// src/routers.js
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from 'react-router-dom';
+
+import MainLayout from './layouts/MainLayout';
+import Auth from './components/Auth';
+import Register from './components/Register';
+import HomePage from './pages/HomePage';
+import GuestRoute from './pages/GuestRoute';
+import PrivateRoute from './pages/PrivateRoute';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route
+        index
+        element={
+          <GuestRoute>
+            <Auth />
+          </GuestRoute>
+        }
+      />
+      <Route
+        path="login"
+        element={
+          <GuestRoute>
+            <Auth />
+          </GuestRoute>
+        }
+      />
+      <Route
+        path="register"
+        element={
+          <GuestRoute>
+            <Register />
+          </GuestRoute>
+        }
+      />
+      <Route
+        path="home"
+        element={
+          <PrivateRoute>
+            <HomePage />
+          </PrivateRoute>
+        }
+      />
+    </Route>
+  )
+);
+
+export default router;
